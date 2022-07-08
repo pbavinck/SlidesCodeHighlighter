946409cfff5623a3ade141f9081a77805c074a98
Peter Bavinck <pbavinck@gmail.com>
2022-07-08 14:49:29 +0200

    bigquery support

--- themes.js
+++ themes.js
@@ -23,6 +23,7 @@ export const THEME_PROPERTIES = [
   { short: 'p', id: 'punctuationColor', name: 'Punctuation', type: 'color' },
   { short: 's', id: 'stringAndValueColor', name: 'Strings, values', type: 'color' },
   { short: 'k', id: 'keywordTagColor', name: 'Keywords, tags', type: 'color' },
+  { short: 'k', id: 'functionColor', name: 'Functions', type: 'color' },
   { short: 'c', id: 'commentColor', name: 'Comments', type: 'color' },
   { short: 'y', id: 'typeColor', name: 'Types', type: 'color' },
   { short: 'n', id: 'numberColor', name: 'Numbers', type: 'color' },
@@ -35,7 +36,7 @@ export const THEME_PROPERTIES = [
 
 export function setTheme(theme, typeSize) {
   let { bgColor, textColor, punctuationColor, stringAndValueColor,
-    keywordTagColor, commentColor, typeColor, numberColor,
+    keywordTagColor, functionColor, commentColor, typeColor, numberColor,
     declarationColor, dimmedColor, highlightColor, lineHeight } = theme;
   lineHeight = lineHeight || 1.5;
   let css = `
@@ -105,6 +106,10 @@ export function setTheme(theme, typeSize) {
       color: ${keywordTagColor};
     }
     
+    #output pre .token.function {
+      color: ${functionColor};
+    }
+
     #output pre .token.important {
       color: ${declarationColor};
     }
@@ -134,6 +139,7 @@ export const DEFAULT_THEMES = {
     punctuationColor: materialColor('blue-grey', '800'),
     stringAndValueColor: materialColor('green', '700'),
     keywordTagColor: materialColor('indigo', '500'),
+    functionColor: materialColor('indigo', '500'),
     commentColor: materialColor('pink', '600'),
     typeColor: materialColor('purple', '500'),
     numberColor: '#c53929', // g-red 700
@@ -148,6 +154,7 @@ export const DEFAULT_THEMES = {
     punctuationColor: '#a3a3a3',
     stringAndValueColor: '#0f9d58',
     keywordTagColor: '#4285f4',
+    functionColor: '#4285f4',
     commentColor: '#999999',
     typeColor: '#673ab7',
     numberColor: '#db4437',
@@ -163,6 +170,7 @@ export const DEFAULT_THEMES = {
     punctuationColor: materialColor('blue-grey', '50'),
     stringAndValueColor: materialColor('light-green', '400'),
     keywordTagColor: materialColor('cyan', '300'),
+    functionColor: materialColor('cyan', '300'),
     commentColor: materialColor('pink', '300'),
     typeColor: materialColor('purple', '200'),
     numberColor: materialColor('yellow', '700'),
@@ -177,6 +185,7 @@ export const DEFAULT_THEMES = {
     punctuationColor: '#a3a3a3',
     stringAndValueColor: '#57bb8a',
     keywordTagColor: '#7baaf7',
+    functionColor: '#7baaf7',
     commentColor: '#aaaaaa',
     typeColor: '#ff8a65', // alt #f06292
     numberColor: '#f4b400',
@@ -191,6 +200,7 @@ export const DEFAULT_THEMES = {
     punctuationColor: '#90a4ae',
     stringAndValueColor: '#00bfa4',
     keywordTagColor: '#26c6da',
+    functionColor: '#26c6da',
     commentColor: '#607d8b',
     typeColor: '#ff8a80',
     numberColor: '#ffbc00',
@@ -205,6 +215,7 @@ export const DEFAULT_THEMES = {
     punctuationColor: '#90a4ae',
     stringAndValueColor: '#1ce8b5',
     keywordTagColor: '#00e4ff',
+    functionColor: '#00e4ff',
     commentColor: '#ff5cb4',
     typeColor: '#ff8857', // ff6d00
     numberColor: '#ffd500',
@@ -219,6 +230,7 @@ export const DEFAULT_THEMES = {
     punctuationColor: '#9aa0a6',
     stringAndValueColor: '#5bb974',
     keywordTagColor: '#669df6',
+    functionColor: '#669df6',
     commentColor: '#9aa0a6',
     typeColor: '#ee675c',
     numberColor: '#fcc934',
@@ -233,6 +245,7 @@ export const DEFAULT_THEMES = {
     punctuationColor: "#8be9fd",
     stringAndValueColor: "#ffa65c",
     keywordTagColor: "#1cdec9",
+    functionColor: "#1cdec9",
     commentColor: "#808080",
     typeColor: "#d65bad",
     numberColor: "#bd93f9",
@@ -247,6 +260,7 @@ export const DEFAULT_THEMES = {
     punctuationColor: materialColor('light-blue', '100'),
     stringAndValueColor: "#c3e88d",
     keywordTagColor: materialColor('light-blue', '100'),
+    functionColor: materialColor('light-blue', '100'),
     commentColor: materialColor('grey', '500'),
     typeColor: materialColor('orange', '200'),
     numberColor: materialColor('purple', '200'),
@@ -254,5 +268,21 @@ export const DEFAULT_THEMES = {
     dimmedColor: "#4d4d4d",
     highlightColor: "#4d3663",
     lineHeight: 1.2
-  }
+  },
+  'bigquery': {
+    bgColor: "#ffffff",
+    textColor: "#444444",
+    punctuationColor: "#888888",
+    stringAndValueColor: "#0d904f",
+    keywordTagColor: "#3367d6",
+    functionColor: "#3367d6",
+    commentColor: "#d81b60",
+    typeColor: "#9C27B0",
+    numberColor: "#f4511e",
+    declarationColor: "#3367d6",
+    dimmedColor: "#BDBDBD",
+    highlightColor: '#E0E0E0',
+    lineHeight: 1.15,
+  },
+
 };
